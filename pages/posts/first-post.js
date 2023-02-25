b44c96f943005f2bb9e3a611556c485a564afef1
br3ntor <br3ntor@gmail.com>
2023-02-25 02:58:53 -0800

    Done for the night.

---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Shelby"
+  />
+);
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post!</h1>
+      <YourComponent />
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
